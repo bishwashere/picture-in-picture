95491e6a7a12c8f04296606f7b89ecf66793acfd
Bishwas Mishra <b.bishwas@gmail.com>
2020-07-11 12:41:37 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -17,10 +17,10 @@ def home(url):
 		embed_url = url+'/embed/'+v
 	else:
 		url_list = url.split('/')
-		print(url, file=sys.stderr)
+		# print(url, file=sys.stderr)
 		embed_url = 'https://youtube.com'  + '/embed/'+  url_list[3]
-		print(embed_url, file=sys.stderr)
-	print(embed_url, file=sys.stderr)
+		# print(embed_url, file=sys.stderr)
+	#print(embed_url, file=sys.stderr)
 	return '<iframe allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" class="embed-responsive-item" src="%s?modestbranding=1&amp;rel=0&amp;showinfo=0" scrolling="no"></iframe>' % embed_url
 
 if __name__ == '__main__':
